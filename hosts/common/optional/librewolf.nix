5d15959674afc97549f796b6e99a4917d36052b2
Sebastian Torres Valenta <s.torresval+github@protonmail.com>
2025-03-27 13:34:11 -0300

    added 2 extensions to librewolf

--- hosts/common/optional/librewolf.nix
+++ hosts/common/optional/librewolf.nix
@@ -9,9 +9,17 @@
         # 2) Under the "Inspector" tab, in the "Search HTML" field, type "guid" and press enter.
         # 3) Right-click the highlighted <script> tag, then select "Copy" > "Inner HTML".
         # 4) Paste the contents on a JSON formatter like https://jsonlint.com/
-        # 5) Get the GUID from line 28.
+        # 5) Search for the GUID field.
         "{446900e4-71c2-419f-a6a7-df9c091e268b}" = {
           install_url = "https://addons.mozilla.org/firefox/downloads/latest/bitwarden-password-manager/latest.xpi";
+          installation_mode = "force_installed";
+        };
+        "addon@darkreader.org" = {
+          install_url = "https://addons.mozilla.org/firefox/downloads/latest/darkreader/latest.xpi";
+          installation_mode = "force_installed";
+        };
+        "{2e5ff8c8-32fe-46d0-9fc8-6b8986621f3c}" = {
+          install_url = "https://addons.mozilla.org/firefox/downloads/latest/search_by_image/latest.xpi";
           installation_mode = "force_installed";
         };
       };
