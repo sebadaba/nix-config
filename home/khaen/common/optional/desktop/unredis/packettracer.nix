a4548aa625545e3d81da21af4f3e677543ad8eb5
Sebastian Torres Valenta <s.torresval+github@protonmail.com>
2025-04-04 17:56:49 -0300

    added window rule for cisco packet tracer in hyprland

--- home/khaen/common/optional/desktop/unredis/packettracer.nix
+++ home/khaen/common/optional/desktop/unredis/packettracer.nix
@@ -1,3 +1,13 @@
-{pkgs, ...}: {
-  home.packages = with pkgs; [ciscoPacketTracer8];
+{
+  pkgs,
+  lib,
+  config,
+  ...
+}: {
+  config = {
+    home.packages = with pkgs; [ciscoPacketTracer8];
+    wayland.windowManager.hyprland.settings.windowrulev2 = lib.mkIf config.wayland.windowManager.hyprland.enable [
+      "float, initialClass:PacketTracer, initialTitle:negative:Cisco Packet Tracer"
+    ];
+  };
 }
