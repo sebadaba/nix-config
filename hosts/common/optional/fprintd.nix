7b84c57158a76e6d16f54903b2e2b0d553e972f8
Sebastian Torres Valenta <s.torresval+github@protonmail.com>
2025-04-04 17:57:15 -0300

    added fingerprint scanner config

---
+++ hosts/common/optional/fprintd.nix
@@ -0,0 +1,8 @@
+{pkgs, ...}: {
+  # Start the driver at boot
+  systemd.services.fprintd = {
+    wantedBy = ["multi-user.target"];
+    serviceConfig.Type = "simple";
+  };
+  services.fprintd.enable = true;
+}
