c05aa3eb9e5341ea697f9c0fa5c82485359511b0
Sebastian Torres Valenta <s.torresval+github@protonmail.com>
2025-04-27 19:44:22 -0400

    vscode profiles + nixfmt

--- home/khaen/common/optional/development/vscode.nix
+++ home/khaen/common/optional/development/vscode.nix
@@ -1,32 +1,54 @@
 {
   pkgs,
-  lib,
   config,
+  lib,
   ...
-}: {
-  config = {
-    programs.vscode = {
-      enable = true;
-      userSettings = {
-        "window.titleBarStyle" = "custom"; # Avoids crash on startup on Hyprland.
-        # Tabs
-        "editor.tabSize" = 4; # Size of 1 tab == Size of 4 spaces.
-        "editor.insertSpaces" = false; # Insert a tab character, not a couple of spaces.
-        "editor.detectIndentation" = true; # If a file has another format, stick with it.
-      };
-      mutableExtensionsDir = false;
+}:
+let
+  profiles = {
+    Base = {
       extensions = with pkgs.vscode-extensions; [
-        jnoortheen.nix-ide
-        kamadorueda.alejandra
-        mechatroner.rainbow-csv
-        ms-vscode.cpptools
-        ms-python.python
-        ms-vscode.makefile-tools
+        esbenp.prettier-vscode # General formatter
+        adpyke.codesnap # Nice screenshots
+        vscode-icons-team.vscode-icons # Better icons
+        jnoortheen.nix-ide # Nix syntax highlight,  formatting..
+        mechatroner.rainbow-csv # CSV colors
       ];
+      userSettings = {
+        "editor.formatOnSave" = true;
+        "nix.enableLanguageServer" = true;
+        "nix.serverPath" = "nixd";
+        "nix.serverSettings" = {
+          "nixd" = {
+            "formatting" = {
+              "command" = [ "nixfmt" ];
+            };
+          };
+        };
+      };
     };
-    home.packages = with pkgs; [
-      alejandra # Nix formatter.
-      sublime-merge
-    ];
+    Java = lib.mkIf config.programs.java.enable {
+      extensions = with pkgs.vscode-extensions; profiles.Base.extensions ++ [ vscjava.vscode-java-pack ];
+    };
   };
+in
+{
+  programs.vscode = {
+    enable = true;
+    userSettings = {
+      "window.titleBarStyle" = "custom"; # Avoids crash on startup on Hyprland.
+      # Tabs
+      "editor.tabSize" = 4; # Size of 1 tab == Size of 4 spaces.
+      "editor.insertSpaces" = false; # Insert a tab character, not a couple of spaces.
+      "editor.detectIndentation" = true; # If a file has another format, stick with it.
+    };
+    mutableExtensionsDir = false;
+    profiles = profiles;
+  };
+  home.packages = with pkgs; [
+    nixfmt-rfc-style # Nix formatter
+    nixd # Nix language server
+    sublime-merge
+  ];
+  stylix.targets.vscode.profileNames = builtins.attrNames profiles;
 }
