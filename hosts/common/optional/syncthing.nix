f06b10b72caad96db472acc87987d4902a6d66e0
Sebastian Torres Valenta <s.torresval+github@protonmail.com>
2025-05-07 21:23:16 -0400

    syncthing.nix, actual finance app thing

---
+++ hosts/common/optional/syncthing.nix
@@ -0,0 +1,13 @@
+{
+  config,
+  ...
+}:
+{
+  services.syncthing = {
+    enable = true;
+    openDefaultPorts = true;
+    user = config.hostAttr.primaryUsername;
+    dataDir = "${config.users.users.${config.hostAttr.primaryUsername}.home}";
+  };
+  systemd.services.syncthing.environment.STNODEFAULTFOLDER = "true"; # Don't create default ~/Sync folder
+}
