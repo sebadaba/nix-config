54d340b41b9aa5ae261663ac662861e81015e8ca
Sebastian Torres Valenta <s.torresval+github@protonmail.com>
2025-05-27 15:59:51 -0400

    bluetooth by default is off

--- hosts/common/optional/bluetooth.nix
+++ hosts/common/optional/bluetooth.nix
@@ -1,14 +1,14 @@
 {
-  pkgs,
   config,
   lib,
   ...
-}: {
+}:
+{
   hardware.bluetooth = {
     enable = true;
-    powerOnBoot = true;
+    powerOnBoot = false;
     settings = {
-      
+
     };
   };
   services.blueman.enable = lib.mkIf config.programs.hyprland.enable true;
