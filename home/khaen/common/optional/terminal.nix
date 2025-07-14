0338055dafdd0afc3b4ed08c9a759efc8726360f
Sebastian Torres Valenta <s.torresval+github@protonmail.com>
2025-07-14 08:37:51 -0400

    added zoxide and macchina

--- home/khaen/common/optional/terminal.nix
+++ home/khaen/common/optional/terminal.nix
@@ -1,13 +1,21 @@
 {
   pkgs,
+  config,
   ...
 }:
 {
   programs = {
     zsh = {
       enable = true;
+      initContent = ''
+        macchina -t custom -o machine,distribution,processor,memory,desktop-environment,terminal,shell,packages,resolution,kernel,battery,uptime
+      '';
     };
     starship = {
+      enable = true;
+      enableZshIntegration = true;
+    };
+    zoxide = {
       enable = true;
       enableZshIntegration = true;
     };
@@ -17,6 +25,16 @@
   };
   home.packages = with pkgs; [
     xpipe
-    neofetch
+    macchina
   ];
+  xdg.configFile = {
+    "macchina/themes/custom.toml".text = ''
+      hide_ascii = true
+
+      [box]
+      title = "${config.hostSpec.primaryUsername}@${config.hostSpec.hostname}"
+      border = "rounded"
+      visible = true
+    '';
+  };
 }
