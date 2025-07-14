7df4b025e0e0c055fa9677801ed1507c75b70175
Sebastian Torres Valenta <s.torresval+github@protonmail.com>
2025-07-14 09:57:41 -0400

    librewolf search engine config

--- home/khaen/common/optional/librewolf.nix
+++ home/khaen/common/optional/librewolf.nix
@@ -1,5 +1,8 @@
 {
+  pkgs,
   inputs,
+  config,
+  lib,
   ...
 }:
 # basically same options as programs.firefox
@@ -23,7 +26,54 @@
       settings = {
         "privacy.resistFingerprinting.letterboxing" = true;
       };
+      search.engines = {
+        nixpkgs = {
+          name = "Nixpkgs search";
+          urls = [
+            {
+              template = "https://search.nixos.org/packages";
+              params = [
+                {
+                  name = "type";
+                  value = "packages";
+                }
+                {
+                  name = "query";
+                  value = "{searchTerms}";
+                }
+              ];
+            }
+          ];
+
+          icon = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
+          definedAliases = [ "@np" ];
+        };
+        hm-options = {
+          name = "Home-Manager options search";
+          urls = [
+            {
+              template = "https://home-manager-options.extranix.com/";
+              params = [
+                {
+                  name = "query";
+                  value = "{searchTerms}";
+                }
+              ];
+            }
+          ];
+
+          icon = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
+          definedAliases = [ "@hm" ];
+        };
+      };
     };
   };
   stylix.targets.librewolf.profileNames = [ "primary" ];
+
+  # https://github.com/nix-community/home-manager/issues/4199#issuecomment-2683440129
+  home.activation.removeBrowserBackups = lib.hm.dag.entryAfter [ "checkLinkTargets" ] ''
+    	if [ -d "${config.home.homeDirectory}/.librewolf/primary" ]; then
+    		rm -f ${config.home.homeDirectory}/.librewolf/primary/search.json.mozlz4.backup
+    	fi
+  '';
 }
