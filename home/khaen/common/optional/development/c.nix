e16944e7c2c035568e68919cd38dbc0ff1ef02ed
Sebastian Torres Valenta <s.torresval+github@protonmail.com>
2025-07-15 12:58:02 -0400

    added qt creator and clion to c.nix

--- home/khaen/common/optional/development/c.nix
+++ home/khaen/common/optional/development/c.nix
@@ -1,13 +1,15 @@
 {
   pkgs,
-  lib,
-  config,
   ...
-}: {
+}:
+{
   home.packages = with pkgs; [
     gcc # GNU Compiler Collection
     gdb # GNU Debugger
     gnumake # GNU's Make
     valgrind
+    qtcreator
+    libsForQt5.full
+    jetbrains.clion
   ];
 }
