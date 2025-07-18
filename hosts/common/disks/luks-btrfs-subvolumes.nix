c0ffa194ef6cb95954500ad1767815c1ac90694b
Sebastian Torres Valenta <s.torresval+github@protonmail.com>
2025-07-18 17:28:04 -0400

    added new host sevastopol, added disko layout

---
+++ hosts/common/disks/luks-btrfs-subvolumes.nix
@@ -0,0 +1,74 @@
+# https://github.com/nix-community/disko/blob/2bf3421f7fed5c84d9392b62dcb9d76ef09796a7/example/luks-btrfs-subvolumes.nix
+{
+  disk ? "/dev/vda",
+}:
+{
+  disko.devices = {
+    disk = {
+      main = {
+        type = "disk";
+        device = disk;
+        content = {
+          type = "gpt";
+          partitions = {
+            ESP = {
+              size = "512M";
+              type = "EF00";
+              content = {
+                type = "filesystem";
+                format = "vfat";
+                mountpoint = "/boot";
+                mountOptions = [ "umask=0077" ];
+              };
+            };
+            luks = {
+              size = "100%";
+              content = {
+                type = "luks";
+                name = "crypted";
+                # disable settings.keyFile if you want to use interactive password entry
+                #passwordFile = "/tmp/secret.key"; # Interactive
+                settings = {
+                  allowDiscards = true;
+                  keyFile = "/tmp/secret.key";
+                };
+                additionalKeyFiles = [ "/tmp/additionalSecret.key" ];
+                content = {
+                  type = "btrfs";
+                  extraArgs = [ "-f" ];
+                  subvolumes = {
+                    "/root" = {
+                      mountpoint = "/";
+                      mountOptions = [
+                        "compress=zstd"
+                        "noatime"
+                      ];
+                    };
+                    "/home" = {
+                      mountpoint = "/home";
+                      mountOptions = [
+                        "compress=zstd"
+                        "noatime"
+                      ];
+                    };
+                    "/nix" = {
+                      mountpoint = "/nix";
+                      mountOptions = [
+                        "compress=zstd"
+                        "noatime"
+                      ];
+                    };
+                    "/swap" = {
+                      mountpoint = "/.swapvol";
+                      swap.swapfile.size = "20M";
+                    };
+                  };
+                };
+              };
+            };
+          };
+        };
+      };
+    };
+  };
+}
---
+++ hosts/nixos/sevastopol/default.nix
@@ -0,0 +1,64 @@
+{
+  config,
+  lib,
+  pkgs,
+  inputs,
+  ...
+}:
+{
+  imports = [
+    #
+    # ========== Hardware ==========
+    #
+    ./hardware-configuration.nix
+    inputs.hardware.nixosModules.common-cpu-intel
+    inputs.hardware.nixosModules.common-pc-ssd
+
+    #
+    # ========== Disk Layout ==========
+    #
+    inputs.disko.nixosModules.disko
+    ../../common/disks/luks-btrfs-subvolumes.nix
+    { disk = "nvme0n1"; }
+    #
+    # ========== Required Configs ==========
+    #
+    ../../common/core
+
+    #
+    # ========== Optional Configs ==========
+    #
+    ../../common/optional/audio.nix
+    ../../common/optional/greetd.nix
+    ../../common/optional/hyprland.nix
+    ../../common/optional/syncthing.nix
+  ];
+  hostSpec = {
+    hostname = "sevastopol";
+    scheme = config.lib.base16.mkSchemeAttrs "${pkgs.base16-schemes}/share/themes/ayu-mirage.yaml";
+  };
+
+  boot = {
+    loader = {
+      systemd-boot = {
+        enable = true;
+        xbootldrMountPoint = "/boot";
+        consoleMode = "2";
+      };
+      efi = {
+        canTouchEfiVariables = true;
+        efiSysMountPoint = "/efi";
+      };
+    };
+  };
+
+  system.stateVersion = "24.11";
+
+  services = {
+    openssh.enable = true;
+    actual = {
+      enable = true;
+      openFirewall = true;
+    };
+  };
+}
