b59f1c7af927b80b97718cdbe65e1ff8b01abfdd
Sebastian Torres Valenta <s.torresval+github@protonmail.com>
2025-09-15 16:37:15 -0300

    nixos hyprland stuff

--- hosts/common/optional/hyprland.nix
+++ hosts/common/optional/hyprland.nix
@@ -4,10 +4,17 @@
     enable = true;
     withUWSM = true;
   };
-  #environment.systemPackages = with pkgs; [xdg-desktop-portal-hyprland];
+  programs.dconf.enable = true;
+  #environment.systemPackages = with pkgs; [ gsettings-desktop-schemas ];
   xdg.portal = {
     enable = true;
-    extraPortals = with pkgs; [ xdg-desktop-portal-hyprland ];
+    extraPortals = [ pkgs.xdg-desktop-portal-hyprland ];
   };
-  environment.sessionVariables.NIXOS_OZONE_WL = "1";
+  environment.sessionVariables = {
+    NIXOS_OZONE_WL = "1";
+    #    XDG_DATA_DIRS = lib.mkAfter [
+    #      "${pkgs.gsettings-desktop-schemas}/share/gsettings-schemas/${pkgs.gsettings-desktop-schemas.name}"
+    #      "${pkgs.gtk3}/share/gsettings-schemas/${pkgs.gtk3.name}"
+    #    ]; # qt5 media player problems
+  };
 }
