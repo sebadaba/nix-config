f3455e13927de392876450449f0578887ff1a0b1
Sebastian Torres Valenta <s.torresval+github@protonmail.com>
2025-09-26 10:35:12 -0300

    feat(greetd): universal config

--- hosts/common/optional/greetd.nix
+++ hosts/common/optional/greetd.nix
@@ -2,12 +2,21 @@
   config,
   ...
 }:
+let
+  command =
+    if config.hostSpec.wlCompositor == "Hyprland" then
+      "uwsm start hyprland-uwsm.desktop"
+    else if config.hostSpec.wlCompositor == "Niri" then
+      "niri-session"
+    else
+      "/bin/sh";
+in
 {
   services.greetd = {
     enable = true;
     settings = rec {
       initial_session = {
-        command = "uwsm start hyprland-uwsm.desktop";
+        command = command;
         user = config.hostSpec.primaryUsername;
       };
       default_session = initial_session;
--- modules/common/host-spec.nix
+++ modules/common/host-spec.nix
@@ -18,5 +18,14 @@
       type = lib.types.attrs;
       description = "Color scheme of the host";
     };
+    wlCompositor = lib.mkOption {
+      type = lib.types.enum [
+        "Hyprland"
+        "Niri"
+        "None"
+      ];
+      default = "None";
+      description = "The Wayland compositor to use on this host, if any";
+    };
   };
 }
