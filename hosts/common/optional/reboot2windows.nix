2c6b500392aba1628e9f92465c22000aeb795153
Sebastian Torres Valenta <s.torresval+github@protonmail.com>
2025-10-17 18:36:23 -0300

    feat(reboot-to-windows):now uses NextBoot UEFI var

--- hosts/common/optional/reboot2windows.nix
+++ hosts/common/optional/reboot2windows.nix
@@ -1,13 +1,19 @@
 {
   pkgs,
-  inputs,
   ...
-}: let
-  reboot2windows = pkgs.writeShellScriptBin "reboot2windows" ''
-    sudo systemctl reboot --boot-loader-entry=auto-windows
-  '';
-in {
+}:
+{
   environment.systemPackages = [
-    reboot2windows
+    (pkgs.writeShellScriptBin "reboot-to-windows" ''
+      ENTRY=$(${pkgs.efibootmgr}/bin/efibootmgr | grep -i "windows" | head -1 | sed 's/Boot\([0-9]*\).*/\1/')
+
+      if [ -z "$ENTRY" ]; then
+        echo "UEFI Windows boot entry not found!"
+        exit 1
+      fi
+
+      echo "Rebooting to Windows.. (Boot$ENTRY)..."
+      ${pkgs.efibootmgr}/bin/efibootmgr -n 0x$ENTRY && systemctl reboot
+    '')
   ];
 }
