aeffb6a814a4e1724fe8f4308fe204cab29a7106
Sebastian Torres Valenta <s.torresval+github@protonmail.com>
2025-11-08 23:09:50 -0300

    feat: plymouth config

---
+++ hosts/common/optional/plymouth.nix
@@ -0,0 +1,28 @@
+{ pkgs, ... }:
+{
+  boot = {
+
+    plymouth = {
+      enable = true;
+      theme = "lone";
+      themePackages = with pkgs; [
+        # By default we would install all themes
+        (adi1090x-plymouth-themes.override {
+          selected_themes = [ "lone" ];
+        })
+      ];
+    };
+
+    # Enable "Silent boot"
+    consoleLogLevel = 3;
+    initrd.verbose = false;
+    kernelParams = [
+      "quiet"
+      "splash"
+      "boot.shell_on_fail"
+      "udev.log_priority=3"
+      "rd.systemd.show_status=auto"
+    ];
+  };
+  stylix.targets.plymouth.enable = false;
+}
