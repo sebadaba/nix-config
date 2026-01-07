9ce5b5f60a2d966f68f744def89e1401b1c94f79
Sebastian Torres Valenta <s.torresval+github@protonmail.com>
2026-01-07 14:41:20 -0300

    refactor: migrated audio aspect to flake-parts

---
+++ modules/audio.nix
@@ -0,0 +1,37 @@
+{
+  pkgs,
+  ...
+}:
+{
+  flake.modules.nixos.audio = {
+    security.rtkit.enable = true;
+    services = {
+      pulseaudio.enable = false;
+      pipewire = {
+        enable = true;
+        package = pkgs.pipewire;
+
+        alsa = {
+          enable = true;
+          support32Bit = true;
+        };
+
+        wireplumber = {
+          enable = true;
+          package = pkgs.wireplumber;
+        };
+
+        pulse.enable = true;
+        jack.enable = true;
+      };
+    };
+  };
+
+  flake.modules.homeManager.audio = {
+    home.packages = with pkgs; [
+      pwvucontrol # PipeWire volume control GUI
+      qpwgraph # PipeWire patchbay/graph manager
+      coppwr # Low level control GUI for PipeWire
+    ];
+  };
+}
