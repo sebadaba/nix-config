ba85b6d591fab1e4a9c35db0d74ec508f3579f7e
Sebastian Torres Valenta <s.torresval+github@protonmail.com>
2026-01-11 15:04:05 -0300

    fix: audio module now evaluates correctly

--- modules/audio.nix
+++ modules/audio.nix
@@ -1,37 +1,38 @@
+{ ... }:
 {
-  pkgs,
-  ...
-}:
-{
-  flake.modules.nixos.audio = {
-    security.rtkit.enable = true;
-    services = {
-      pulseaudio.enable = false;
-      pipewire = {
-        enable = true;
-        package = pkgs.pipewire;
-
-        alsa = {
+  flake.modules.nixos.audio =
+    { pkgs, ... }:
+    {
+      security.rtkit.enable = true;
+      services = {
+        pulseaudio.enable = false;
+        pipewire = {
           enable = true;
-          support32Bit = true;
-        };
+          package = pkgs.pipewire;
 
-        wireplumber = {
-          enable = true;
-          package = pkgs.wireplumber;
-        };
+          alsa = {
+            enable = true;
+            support32Bit = true;
+          };
 
-        pulse.enable = true;
-        jack.enable = true;
+          wireplumber = {
+            enable = true;
+            package = pkgs.wireplumber;
+          };
+
+          pulse.enable = true;
+          jack.enable = true;
+        };
       };
     };
-  };
 
-  flake.modules.homeManager.audio = {
-    home.packages = with pkgs; [
-      pwvucontrol # PipeWire volume control GUI
-      qpwgraph # PipeWire patchbay/graph manager
-      coppwr # Low level control GUI for PipeWire
-    ];
-  };
+  flake.modules.homeManager.audio =
+    { pkgs, ... }:
+    {
+      home.packages = with pkgs; [
+        pwvucontrol # PipeWire volume control GUI
+        qpwgraph # PipeWire patchbay/graph manager
+        coppwr # Low level control GUI for PipeWire
+      ];
+    };
 }
--- modules/hosts/romulus/configuration.nix
+++ modules/hosts/romulus/configuration.nix
@@ -7,6 +7,7 @@
     imports =
       with inputs.self.modules.nixos;
       [
+        audio
         plymouth
         #...
       ]
