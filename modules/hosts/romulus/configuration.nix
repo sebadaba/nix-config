9b852cc488398c90190c61571bb515d6d0ba1d7b
Sebastian Torres Valenta <s.torresval+github@protonmail.com>
2026-01-26 14:32:44 -0300

    feat(romulus): firmware config, satdump & sdrpp
    also added config for VM created with
    nixos-rebuild build-vm so it runs with
    2 cpus and 4GB RAM

--- modules/hosts/romulus/configuration.nix
+++ modules/hosts/romulus/configuration.nix
@@ -3,26 +3,45 @@
   ...
 }:
 {
-  flake.modules.nixos.romulus = {
-    imports =
-      with inputs.self.modules.nixos;
-      [
-        audio
-        plymouth
-        #...
-      ]
-      ++ [ inputs.self.modules.generic.constants ];
+  flake.modules.nixos.romulus =
+    { pkgs, ... }:
+    {
+      imports =
+        with inputs.self.modules.nixos;
+        [
+          audio
+          plymouth
+          #...
+        ]
+        ++ [ inputs.self.modules.generic.constants ];
 
-    services = {
-      openssh.enable = true;
-      tailscale.enable = true;
+      services = {
+        openssh.enable = true;
+        tailscale.enable = true;
+      };
+
+      networking = {
+        hostName = "romulus";
+        networkmanager.enable = true;
+      };
+
+      system.stateVersion = "24.11";
+
+      virtualisation.vmVariant = {
+        virtualisation = {
+          memorySize = 4096;
+          cores = 2;
+        };
+
+      };
+
+      hardware = {
+        graphics.enable = true;
+        enableAllFirmware = true;
+        #enableRedistributableFirmware = true;
+        firmware = [ pkgs.linux-firmware ];
+      };
+
+      nixpkgs.config.allowUnfree = true;
     };
-
-    networking = {
-      hostName = "romulus";
-      networkmanager.enable = true;
-    };
-
-    system.stateVersion = "24.11";
-  };
 }
--- modules/hosts/romulus/users/khaen.nix
+++ modules/hosts/romulus/users/khaen.nix
@@ -3,13 +3,23 @@
   ...
 }:
 {
-  flake.modules.nixos.romulus = {
-    imports = with inputs.self.modules.nixos; [
-      khaen
-    ];
+  flake.modules.nixos.romulus =
+    { pkgs, ... }:
+    {
+      imports =
+        with inputs.self.modules.nixos;
+        [
+          khaen
+        ]
+        ++ [ inputs.home-manager.nixosModules.home-manager ];
 
-    home-manager.users.khaen = {
-      # Specific config for khaen on romulus
+      home-manager.users.khaen = {
+        # Specific config for khaen on romulus
+        home.packages = with pkgs; [
+          satdump
+          sdrpp
+        ];
+        home.stateVersion = "24.11";
+      };
     };
-  };
 }
