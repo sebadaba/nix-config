ed5ba61db68de78fc9812cc889fc2d3c37860faa
Sebastián Torres <s.torresval@protonmail.com>
2026-07-07 16:48:01 -0400

    feat: onlyoffice
    removed libreoffice in favor of onlyoffice

--- modules/programs/gui/desktop-env/multimedia/office.nix
+++ modules/programs/gui/desktop-env/multimedia/office.nix
@@ -3,23 +3,26 @@
     { pkgs, ... }:
     {
       home.packages = with pkgs; [
-        libreoffice
+        onlyoffice-desktopeditors
       ];
 
       xdg.mimeApps.defaultApplications = {
         # Text
-        "application/vnd.oasis.opendocument.text" = "writer.desktop"; # .odt
-        "application/vnd.openxmlformats-officedocument.wordprocessingml.document" = "writer.desktop"; # .docx
-        "application/msword" = "writer.desktop"; # .doc
+        "application/vnd.oasis.opendocument.text" = "onlyoffice-desktopeditors.desktop"; # .odt
+        "application/vnd.openxmlformats-officedocument.wordprocessingml.document" =
+          "onlyoffice-desktopeditors.desktop"; # .docx
+        "application/msword" = "onlyoffice-desktopeditors.desktop"; # .doc
         # Spreadsheets
-        "application/vnd.oasis.opendocument.spreadsheet" = "calc.desktop"; # .ods
-        "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet" = "calc.desktop"; # .xlsx
-        "application/vnd.ms-excel" = "calc.desktop"; # .xls
-        "text/csv" = "calc.desktop"; # .csv
+        "application/vnd.oasis.opendocument.spreadsheet" = "onlyoffice-desktopeditors.desktop"; # .ods
+        "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet" =
+          "onlyoffice-desktopeditors.desktop"; # .xlsx
+        "application/vnd.ms-excel" = "onlyoffice-desktopeditors.desktop"; # .xls
+        "text/csv" = "onlyoffice-desktopeditors.desktop"; # .csv
         # Presentations
-        "application/vnd.oasis.opendocument.presentation" = "impress.desktop"; # .odp
-        "application/vnd.openxmlformats-officedocument.presentationml.presentation" = "impress.desktop"; # .pptx
-        "application/vnd.ms-powerpoint" = "impress.desktop"; # .ppt
+        "application/vnd.oasis.opendocument.presentation" = "onlyoffice-desktopeditors.desktop"; # .odp
+        "application/vnd.openxmlformats-officedocument.presentationml.presentation" =
+          "onlyoffice-desktopeditors.desktop"; # .pptx
+        "application/vnd.ms-powerpoint" = "onlyoffice-desktopeditors.desktop"; # .ppt
       };
     };
 }
