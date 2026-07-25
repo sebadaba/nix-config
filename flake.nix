07512b18f0ec2faf45cf351b7fc54523d3e24a12
Sebastián Torres <s.torresval@protonmail.com>
2026-07-25 11:33:24 -0400

    broken flake update

--- flake.nix
+++ flake.nix
@@ -47,7 +47,7 @@
     };
 
     noctalia = {
-      url = "github:noctalia-dev/noctalia-shell";
+      url = "github:noctalia-dev/noctalia/legacy-v4";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
