9e8f79cb8a2e1e2aafd4198e627ae714dd5faa1c
Sebastián Torres <s.torresval@protonmail.com>
2026-08-06 19:15:23 -0400

    feat(nostromo): sunshine

--- modules/hosts/nostromo/configuration.nix
+++ modules/hosts/nostromo/configuration.nix
@@ -25,6 +25,16 @@
         tuned.enable = true;
         openssh.enable = true;
         tailscale.enable = true;
+        sunshine = {
+          enable = true;
+          autoStart = true;
+          capSysAdmin = true; # only needed for Wayland -- omit this when using with Xorg
+          openFirewall = true;
+        };
+      };
+
+      users.users.khaen = {
+        extraGroups = [ "uinput" ];
       };
 
       networking = {
