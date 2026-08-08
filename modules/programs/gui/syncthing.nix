48cab8b1fd724bda08ad5bde60901edbc9a5ac00
Sebastián Torres <s.torresval@protonmail.com>
2026-08-08 17:59:37 -0400

    chore(syncthing): disabled tray icon
    until I fix an annoying error

--- modules/programs/gui/syncthing.nix
+++ modules/programs/gui/syncthing.nix
@@ -2,7 +2,9 @@
   flake.modules.homeManager.syncthing = {
     services.syncthing = {
       enable = true;
-      tray.enable = true; # Configuration wizard opens on first launch!
+      # Configuration wizard opens on first launch!
+      # TODO: Fix synchting tray "Unable to request syncthing config" error
+      #tray.enable = true;
       overrideDevices = false;
       overrideFolders = false;
       settings = {
